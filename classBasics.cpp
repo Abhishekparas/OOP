3c216e2255b3da080898a2f62cfefae57cbe9c0a
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-08-28 23:11:45 +0530

    Add files via upload

---
+++ classBasics.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class User {
+public:
+	string firstName;
+	string lastName;
+
+	string getStatus() {
+		return status;
+	}
+
+private:
+	string status = "Online";
+};
+
+int main(int argc, char const *argv[])
+{
+	User user;
+	user.firstName = "abhishek";
+	user.lastName = "parashar";
+
+	cout << "First name: " << user.firstName << endl;
+	cout << "Last name: " << user.lastName << endl;
+	cout << "Status : " << user.getStatus() << endl;
+
+	return 0;
+}
